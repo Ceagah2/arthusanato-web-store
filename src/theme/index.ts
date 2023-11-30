a3b8317a6395e503997dbde54846a3791168bfae
Carlos Ceagah <carlosceagah@gmail.com>
2023-11-30 16:19:14 -0300

    style: add fonts and create theme sheet

---
+++ src/theme/index.ts
@@ -0,0 +1,40 @@
+export const theme = {
+  colors: {
+    //Tons Terrosos e Naturais:
+    brown: '#654321',
+    beige: '#ECE0B7',
+    darkGreen: '#556B2F',
+    terracota: '#CC5533',
+
+    // Combinações de Paleta de Terra e Acentos Vibrantes:
+    lightBlue: '#00CED1',
+    darkOrange: '#FF8000',
+    mustard: '#D4AF37',
+    deepPurple: '#800080',
+
+    // tons pasteis
+    blue: '#87CEEB',
+    gray: '#D3D3D3',
+    green: '#7FFFD4',
+    pink: '#FFB6C1'
+  },
+  fonts:{
+    heading: '"Dancing Script", cursive',
+    body: 'Montserrat, sans-serif'
+  },
+  fontSizes:{
+    heading: '2rem',
+    subHeading: '1.75rem',
+    title: '1.5rem',
+    body: '1rem',
+    description: '0.75rem',
+  },
+  fontWeights: {
+    thin: 100,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    bold: 700,
+    extraBold: 800
+  }
+}
